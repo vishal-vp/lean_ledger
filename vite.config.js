c1ca94f48784045b4dbbe0447cc5116df1dda4f9
Vishal V Poojary <vishalvp0942@gmail.com>
2024-08-15 16:45:51 +0530

    Add github pages config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr({ svgrOptions: { icon: true } })],
+  build: {
+    outDir: "./build",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
