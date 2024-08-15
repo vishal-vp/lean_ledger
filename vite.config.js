58987d9838a9603b1d69f9bb65646e24f45b2efa
Vishal V Poojary <vishalvp0942@gmail.com>
2024-08-15 17:17:35 +0530

    Enable PWA

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ svgrOptions: { icon: true } })],
+  plugins: [
+    react(),
+    svgr({ svgrOptions: { icon: true } }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Lean Ledger",
+        short_name: "Lean Ledger",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/logo.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/lean_ledger",
   build: {
     outDir: "./build",
